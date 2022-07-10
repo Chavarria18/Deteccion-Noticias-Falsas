940b8ad70dbb5bf9b860a70de3df427718ee4d3e
Chavarria18 <chavarria181386@unis.edu.gt>
2022-07-09 18:09:27 -0600

    requirement.txt

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"chavarria181386@unis.edu.gt\"\n\
-" > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
