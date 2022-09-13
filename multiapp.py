3472d117aa7f2082ba8ea0335d6ef86f9b7fd7ef
Chavarria18 <chavarria181386@unis.edu.gt>
2022-09-13 12:58:27 -0600

    a

--- multiapp.py
+++ multiapp.py
@@ -3,7 +3,7 @@
 from select import select
 import streamlit as st
 import streamlit.components.v1 as components
-from streamlit_option_menu import option_menu
+
 class MultiApp:
     """Framework for combining multiple streamlit applications.
     Usage:
